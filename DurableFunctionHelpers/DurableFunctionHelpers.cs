28f0bdacbdbb99589b5242926b4d7a125ab440ae
Stuart Leeks <stuartle@microsoft.com>
2018-02-05 15:57:37 +0100

    Add return type

--- DurableFunctionHelpers/DurableFunctionHelpers.cs
+++ DurableFunctionHelpers/DurableFunctionHelpers.cs
@@ -5,13 +5,13 @@ namespace DurableFunctionHelpers // TODO - generate this dynamically
 {
     public static class ActivityFunctionHelpers
 	{
-		public static Task<string> SayHello(this DurableOrchestrationContext context) // TODO parameters and return type
+		public static Task<System.String> SayHello(this DurableOrchestrationContext context) // TODO parameters
         {
-            return context.CallActivityAsync<string>("Function1_Hello");
+            return context.CallActivityAsync<System.String>("Function1_Hello");
         }
-		public static Task<string> SayHello2(this DurableOrchestrationContext context) // TODO parameters and return type
+		public static Task<System.String> SayHello2(this DurableOrchestrationContext context) // TODO parameters
         {
-            return context.CallActivityAsync<string>("Function2_Hello");
+            return context.CallActivityAsync<System.String>("Function2_Hello");
         }
 	}
 }
