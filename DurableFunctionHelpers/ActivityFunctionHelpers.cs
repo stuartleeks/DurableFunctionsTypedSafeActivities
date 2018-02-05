0b841ad225d2d24850d41f0ed191c65fffddffe5
Stuart Leeks <stuartle@microsoft.com>
2018-02-05 16:07:45 +0100

    Add parameters

--- DurableFunctionHelpers/ActivityFunctionHelpers.cs
+++ DurableFunctionHelpers/ActivityFunctionHelpers.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace DurableFunctionHelpers
 {
-    public static class ActivityFunctionHelpers_ManuallyCreated
-    {
-        // Idea is that this file would be auto-generated
-        // Look for functions with ActivityTrigger attribute
-        // Would need to think about ignoring "TextWriter log" etc 
-        // Also, should the function name here map to the FunctionName attribute value or the C# function name?
-        public static Task<string> SayHello(this DurableOrchestrationContext context, string name)
-        {
-            return context.CallActivityAsync<string>("Function1_Hello", name);
-        }
-    }
+    //public static class ActivityFunctionHelpers_ManuallyCreated
+    //{
+    //    // Idea is that this file would be auto-generated
+    //    // Look for functions with ActivityTrigger attribute
+    //    // Would need to think about ignoring "TextWriter log" etc 
+    //    // Also, should the function name here map to the FunctionName attribute value or the C# function name?
+    //    public static Task<string> SayHello(this DurableOrchestrationContext context, string name)
+    //    {
+    //        return context.CallActivityAsync<string>("Function1_Hello", name);
+    //    }
+    //}
 }
--- DurableFunctionHelpers/DurableFunctionHelpers.cs
+++ DurableFunctionHelpers/DurableFunctionHelpers.cs
@@ -5,11 +5,11 @@ namespace DurableFunctionHelpers // TODO - generate this dynamically
 {
     public static class ActivityFunctionHelpers
 	{
-		public static Task<System.String> SayHello(this DurableOrchestrationContext context) // TODO parameters
+		public static Task<System.String> SayHello(this DurableOrchestrationContext context , System.String name) 
         {
             return context.CallActivityAsync<System.String>("Function1_Hello");
         }
-		public static Task<System.String> SayHello2(this DurableOrchestrationContext context) // TODO parameters
+		public static Task<System.String> SayHello2(this DurableOrchestrationContext context , System.String name, System.Int32 age) 
         {
             return context.CallActivityAsync<System.String>("Function2_Hello");
         }
